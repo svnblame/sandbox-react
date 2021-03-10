d4018cf5995bf8023480d64ace0f2e0a1314b986
Gene Kelley <eugenek@guildmortgage.net>
2021-03-10 13:23:03 -0800

    Place Footer componenet in separate file.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
-import React, { useState } from "react";
-import {render} from "react-dom";
+import React from "react";
+import Footer from "./Footer.js";
 import './App.css';
 
-function getFooter() {
-  const currentYear = new Date().getFullYear();
-
-  return (
-      <>
-          <h3>Your online supermarket</h3>
-          <ul>
-              <li>Contact</li>
-              <li>Press</li>
-              <li>About</li>
-          </ul>
-          <p>All rights reserved &copy; {currentYear}</p>
-      </>
-  );
-}
-
 function App() {
-  const root = document.querySelector("#root");
-
-  let element = getFooter();
-
-  render(element, root);
-
   return (
     <div className="App">
       <header className="App-header">
         <div id="root"></div>
-        <div id="payment-method"></div>
       </header>
+      <>
+        <Footer />
+      </>
     </div>
   );
 }
