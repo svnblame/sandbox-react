457252592c823748c05b5c4432fd0103cda6774a
Gene Kelley <eugenek@guildmortgage.net>
2021-03-09 16:14:01 -0800

    Finished through chapter 6. https://react-tutorial.app/app.html\?id\=374

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import {render} from "react-dom";
 import './App.css';
 
+function getFooter() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+      <>
+          <h3>Your online supermarket</h3>
+          <ul>
+              <li>Contact</li>
+              <li>Press</li>
+              <li>About</li>
+          </ul>
+          <p>All rights reserved &copy; {currentYear}</p>
+      </>
+  );
+}
+
 function App() {
+  const root = document.querySelector("#root");
+
+  let element = getFooter();
+
+  render(element, root);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="root"></div>
+        <div id="payment-method"></div>
       </header>
     </div>
   );
